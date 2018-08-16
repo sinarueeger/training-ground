dc340dec5845424525c022672de51b632a24e905
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-16 14:40:11 +0200

    adding pre prepared files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# training-ground
-make leaflets and maps of training tracks for CX and MTB
+# Training grounds
+
+Make pretty leaflets and maps of training tracks for CX and MTB.
+
+## CX tracks in Lausanne (CH) area
+
+Code [here](map-cx/map-cx-ls.R).
+
+<iframe src="map-cx/map-cx-ls.html"> </iframe>
---
+++ map-cx/map-cx-ls.R
@@ -0,0 +1,29 @@
+library(googlesheets)
+(my_sheets <- gs_ls())
+tmp <- gs_title("CX tracks in Lausanne")
+coord <- tmp %>%
+  gs_read(ws = "Sheet1")
+
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lat=coord$latitude, lng=coord$longitude, popup=coord$name) %>%
+  # Base groups
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles(providers$MtbMap, group = "MTBmap") %>%
+  addProviderTiles(providers$Thunderforest.OpenCycleMap, group = "CyclingMap") %>%
+  addProviderTiles(providers$OpenTopoMap, group = "Topo") %>%
+  addProviderTiles(providers$HikeBike.HikeBike, group = "HikeBike") %>%
+  addProviderTiles(providers$Stamen.Watercolor, group = "Watercolor") %>%
+  # Layers control
+  addLayersControl(
+    baseGroups = c("OSM (default)", "MTBmap", "CyclingMap", "Topo", "HikeBike", "Watercolor"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+m  # Print the map
+
+
+library(htmlwidgets)
+saveWidget(m, file="map-cx-ls.html")
