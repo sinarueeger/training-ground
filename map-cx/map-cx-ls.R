2ffab9d60c541d81451fb6da49af3b4c76777f09
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-16 16:57:06 +0200

    changed the label color + added an icon

--- map-cx/map-cx-ls.R
+++ map-cx/map-cx-ls.R
@@ -1,14 +1,33 @@
+library(tidyverse)
+
+## get data
+## ----------
 library(googlesheets)
 (my_sheets <- gs_ls())
 tmp <- gs_title("CX tracks in Lausanne")
 coord <- tmp %>%
   gs_read(ws = "Sheet1")
 
-library(leaflet)
+## split into two clusters
+coord.chaletagobet <- coord %>% filter(cluster == "Connection Chalet a Gobet")
+coord.rest <- coord %>% filter(cluster != "Connection Chalet a Gobet")
 
+
+## load icons
+## ---------
+## from here: https://github.com/bhaskarvk/leaflet/blob/master/inst/examples/awesomeMarkers.R
+icon.bike.blue <- makeAwesomeIcon(icon = 'bicycle', markerColor = 'blue', library='fa',
+                           iconColor = 'white')
+icon.bike.green <- makeAwesomeIcon(icon = 'bicycle', markerColor = 'green', library='fa',
+                                  iconColor = 'white')
+
+## make map
+## ---------
+library(leaflet)
 m <- leaflet() %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lat=coord$latitude, lng=coord$longitude, popup=coord$name) %>%
+  addAwesomeMarkers(lat=coord.rest$latitude, lng=coord.rest$longitude, popup=coord.rest$name, icon = icon.bike.blue) %>%
+  addAwesomeMarkers(lat=coord.chaletagobet$latitude, lng=coord.chaletagobet$longitude, popup=coord.chaletagobet$name, icon = icon.bike.green) %>%
   # Base groups
   addTiles(group = "OSM (default)") %>%
   addProviderTiles(providers$MtbMap, group = "MTBmap") %>%
